e911f5d792f5dd202760d4834767b9070fb4720d
sergiopoliveira <sergiopoliveira@outlook.com>
2019-12-07 16:24:26 +0000

    Spring Data JPA Configuration

---
+++ src/main/java/com/sergio/springstatemachine/repository/PaymentRepository.java
@@ -0,0 +1,6 @@
+package com.sergio.springstatemachine.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<PaymentRepository, Long> {
+}
